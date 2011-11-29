4ff8c3ab70a06b317790b0918245eb992131209d
Ben Batschelet <bartleby84@gmail.com>
2011-11-28 23:42:34 -0600

    Temp partials for layout purposes

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,11 +1,14 @@
 class PagesController < ApplicationController
   def home
+    @page_title = "Home"
   end
 
   def about
+    @page_title = "About"
   end
 
   def help
+    @page_title = "Help"
   end
 
 end
