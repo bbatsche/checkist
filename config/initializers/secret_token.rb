466b78aa0163ed26ec80bad671761acc974dfafe
Ben Batschelet <bartleby84@gmail.com>
2011-11-28 23:07:11 -0600

    Initial Commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Checkist::Application.config.secret_token = '232d18b065b8bea5739c715c79bd01e792cc9dd9019d92c2478218d20fe9a78e94173eaf58e94bb0cab91747a8d1b74e204345d285185c6e5c20b9f1b83d6d4a'
