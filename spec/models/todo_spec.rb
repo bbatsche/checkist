3043edff6f9a4e3ba9d40b32c9fb876f448c59d8
Ben Batschelet <bartleby84@gmail.com>
2011-12-09 23:53:20 -0600

    Initial todo spec

--- spec/models/todo_spec.rb
+++ spec/models/todo_spec.rb
@@ -1,5 +1,17 @@
 require 'spec_helper'
 
 describe Todo do
-  pending "add some examples to (or delete) #{__FILE__}"
+  before (:each) do
+    @creator_user = User.create!({ :name => 'John Doe', :email => 'jdoe@cs.utsa.edu', :password => 'john1' })
+    @attr = { :content => 'Todo item' }
+  end
+  
+  it "should create a todo with valid content" do
+    @creator_user.assigned_todos.create(@attr)
+  end
+  
+  it "should require content" do
+    no_content_todo = @creator_user.assigned_todos.create(@attr.merge(:content => ''))
+    no_content_todo.should_not be_valid
+  end
 end
