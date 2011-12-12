9bc7dbccecaac0783e001111faa408cc3acb217c
Ben Batschelet <bartleby84@gmail.com>
2011-12-12 00:42:58 -0600

    Remove static name, make sign up form actually work

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -10,6 +10,7 @@ class UsersController < ApplicationController
     @user = User.new(params[:user])
     
     if @user.save
+      sign_in @user
       redirect_to root_path
     else
       @page_title = "Sign Up"
